129b6f78c290f4aab5f08a94a0ee910c8087edf0
katembu <mose.kate@gmail.com>
2013-05-24 22:40:12 +0300

    initial proportion commit. should return numerator and denominator

--- indicator.py
+++ indicator.py
@@ -42,39 +42,55 @@ class BambooIndicator(object):
         assert 'value' in indicator
         assert isinstance(indicator, dict)
         value = indicator['value']
+        results = 0
         if 'sum' in value:
-            sum_value = 0
-            for v in value['sum']:
-                dataset_id = v['dataset_id']
-                # dataset_id form sources.json is most recent
-                if dataset_id != self._sources[v['source']]\
-                        and self._sources[v['source']] != "":
-                    dataset_id = self._sources[v['source']]
-                dataset = Dataset(
-                    dataset_id=dataset_id, connection=self.connection)
-                if 'calculation' in v:
-                    # check or create calculations
-                    if isinstance(v['calculation'], list):
-                        for calculation in v['calculation']:
-                            calc_exists = self._calculation_exists(
-                                calculation.name, dataset)
-                            if not calc_exists:
-                                dataset.add_calculation(
-                                    name=calculation['name'],
-                                    formula=calculation['formula'])
-                    if isinstance(v['calculation'], dict):
-                        if not self._calculation_exists(
-                                v['calculation']['name'], dataset):
-                            calculation = v['calculation']
+            results = self._get_sum('sum', value, period)
+
+        if 'proportion' in value:
+            proportion = value['proportion']
+            denominator = numerator = 0
+            if 'denominator' in proportion:
+                denominator = self._get_sum('denominator', proportion, period)
+            if 'numerator' in proportion:
+                numerator = self._get_sum('numerator', proportion, period)
+
+            results = {"numerator": numerator, "denominator": denominator}
+
+        return results
+
+    def _get_sum(self, key, value, period):
+        sum_value = 0
+        for v in value[key]:
+            dataset_id = v['dataset_id']
+            # dataset_id form sources.json is most recent
+            if dataset_id != self._sources[v['source']]\
+                    and self._sources[v['source']] != "":
+                dataset_id = self._sources[v['source']]
+            dataset = Dataset(
+                dataset_id=dataset_id, connection=self.connection)
+            if 'calculation' in v:
+                # check or create calculations
+                if isinstance(v['calculation'], list):
+                    for calculation in v['calculation']:
+                        calc_exists = self._calculation_exists(
+                            calculation.name, dataset)
+                        if not calc_exists:
                             dataset.add_calculation(
                                 name=calculation['name'],
                                 formula=calculation['formula'])
-                if 'query' in v:
-                    query_string = json.dumps(v['query'])
-                    template = Template(query_string)
-                    query_string = template.render(period=period)
-                    v['query'] = json.loads(query_string)
-                if 'count' in v and 'query' in v:
-                    sum_value += dataset.get_data(
-                        query=v['query'], count=v['count'])
-            return sum_value
+                if isinstance(v['calculation'], dict):
+                    if not self._calculation_exists(
+                            v['calculation']['name'], dataset):
+                        calculation = v['calculation']
+                        dataset.add_calculation(
+                            name=calculation['name'],
+                            formula=calculation['formula'])
+            if 'query' in v:
+                query_string = json.dumps(v['query'])
+                template = Template(query_string)
+                query_string = template.render(period=period)
+                v['query'] = json.loads(query_string)
+            if 'count' in v and 'query' in v:
+                sum_value += dataset.get_data(
+                    query=v['query'], count=v['count'])
+        return sum_value
