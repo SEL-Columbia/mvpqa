23e80d7b7ee02e4cfea3730c7e830a95a1dcdf57
Ukang'a Dickson <ukanga@gmail.com>
2013-05-30 12:05:11 +0300

    adding proportion_of_over5_with_positive_rdt_received_adt indicator: use python definitions

---
+++ pydefinitions/num_antimalarials_other.py
@@ -0,0 +1,87 @@
+import json
+import sys
+import os
+
+from bamboo.models.dataset import Dataset
+from bamboo.models.observation import Observation
+from bamboo.core.frame import DATASET_ID
+
+from bson.code import Code
+from jinja2 import Template
+
+sys.path.append(
+    os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..'))
+)
+from mvpqa.periods import Period
+
+
+class Definition(object):
+    mapper_str = """
+    function(){
+        value = this;
+        key = this['{{case__case_id}}'];
+        if(isNaN(value["{{num_antimalarials_other}}"])){
+            value["{{num_antimalarials_other}}"] = 0;
+        }
+        emit(key,  value);
+    }
+    """
+
+    reducer_str = """
+    function(key, values){
+        values.sort(function(a, b){
+            a = a["{{meta_timeend}}"];
+            b = b["{{meta_timeend}}"];
+            return a > b? -1: a < b? 1: 0;
+        });
+        var reducedValue = values[0];
+        if(isNaN(reducedValue["{{num_antimalarials_other}}"])){
+            reducedValue["{{num_antimalarials_other}}"] = 0;
+        }
+        return reducedValue;
+    }
+    """
+
+    query_str = """
+    {"{{dataset_id_field}}": "{{dataset.dataset_id}}",
+    "{{meta_timeend}}": {
+                "$gte": "{{period.start}}",
+                "$lte": "{{period.end}}"
+                }
+    }
+    """
+    aggregate_str = """
+    {"$group":
+    {"_id": 0, "total": {"$sum": "$value.{{num_antimalarials_other}}"}}}
+    """
+
+    def __init__(self, db, dataset_id=None, dataset=None):
+        self._db = db
+        if dataset_id:
+            self.dataset = Dataset.find_one(dataset_id)
+        if dataset:
+            self.dataset = dataset
+        if not dataset_id and not dataset:
+            raise Exception(u"Please specify a dataset_id")
+
+    def get_value(self, period):
+        value = None
+        if self.dataset:
+            fields = Observation.encoding(self.dataset)
+            fields["dataset"] = self.dataset
+            fields['dataset_id_field'] = fields[DATASET_ID]
+            fields['period'] = Period.month_period(2013, 3)
+            mapper = Code(Template(self.mapper_str).render(fields))
+            reducer = Code(Template(self.reducer_str).render(fields))
+            query = json.loads(Template(self.query_str).render(fields))
+            query['%(meta_timeend)s' % fields]['$gte'] = period.start
+            query['%(meta_timeend)s' % fields]['$lte'] = period.end
+            aggregate = json.loads(Template(self.aggregate_str).render(fields))
+            results = self._db.observations.map_reduce(
+                mapper, reducer, 'myresults_malaria', query=query)
+            value = None
+            if results.count():
+                aggregate_value = results.aggregate(aggregate)
+                value = aggregate_value['result'][0]['total']
+            self._db.myresults_malaria.drop()
+        return value
--- tests/test_mvp_indicators.py
+++ tests/test_mvp_indicators.py
@@ -196,3 +196,11 @@ class TestMvpIndicator(unittest.TestCase):
         value = self.bamboo_indicator\
             .get_indicator_value(self.indicator, self.period)
         self.assertEqual(value, 0.0)
+
+    def test_proportion_of_over5_with_positive_rdt_receiving_adt(self):
+        self.period = Period.month_period(2013, 3)
+        self._load_json_indicator('proportion_of_over5_with_positive_rdt_'
+                                  'received_adt')
+        value = self.bamboo_indicator\
+            .get_indicator_value(self.indicator, self.period)
+        self.assertEqual(value, 96.51)
