02cfd888ceb433ed0e02b9dfe18d4242a41e12de
Ukang'a Dickson <ukanga@gmail.com>
2013-05-31 10:51:44 +0300

    finalized proportion_of_women_using_modern_fp indicator implementation

---
+++ pydefinitions/proportion_of_women_using_modern_fp.py
@@ -0,0 +1,32 @@
+import sys
+import os
+
+from bamboo.models.dataset import Dataset
+
+sys.path.append(
+    os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..'))
+)
+from mvpqa.pydefinitions import num_using_fp
+from mvpqa.pydefinitions import num_eligible_couples
+
+
+class Definition(object):
+    def __init__(self, db, dataset_id=None, dataset=None):
+        self._db = db
+        if dataset_id:
+            self.dataset = Dataset.find_one(dataset_id)
+        if dataset:
+            self.dataset = dataset
+        if not dataset_id and not dataset:
+            raise Exception(u"Please specify a dataset_id")
+
+    def get_value(self, period):
+        value = None
+        def1 = num_using_fp.Definition(
+            self._db, dataset=self.dataset)
+        numerator = def1.get_value(period)
+        def2 = num_eligible_couples.Definition(
+            self._db, dataset=self.dataset)
+        denominator = def2.get_value(period)
+        value = round(100 * (float(numerator) / float(denominator)), 2)
+        return value
--- tests/test_mvp_indicators.py
+++ tests/test_mvp_indicators.py
@@ -212,3 +212,10 @@ class TestMvpIndicator(unittest.TestCase):
         value = self.bamboo_indicator\
             .get_indicator_value(self.indicator, self.period)
         self.assertEqual(value, -25.0)
+
+    def test_proportion_of_women_using_modern_fp(self):
+        self.period = Period.month_period(2013, 3)
+        self._load_json_indicator('proportion_of_women_using_modern_fp')
+        value = self.bamboo_indicator\
+            .get_indicator_value(self.indicator, self.period)
+        self.assertEqual(value, 100)
