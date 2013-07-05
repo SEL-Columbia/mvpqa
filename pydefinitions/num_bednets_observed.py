a7da120793044cd7758bb052bed68b6bf1e47fa0
katembu <mose.kate@gmail.com>
2013-07-05 09:26:42 +0300

    new indicators

---
+++ pydefinitions/num_bednets_observed.py
@@ -0,0 +1,63 @@
+import json
+
+from bamboo.models.dataset import Dataset
+from bamboo.models.observation import Observation
+from bamboo.core.frame import DATASET_ID
+
+from jinja2 import Template
+
+
+class Definition(object):
+
+    query_str = """
+    {"{{dataset_id_field}}": "{{dataset.dataset_id}}",
+    "{{meta_timeend}}": {
+                "$gte": "{{period.start}}",
+                "$lte": "{{period.end}}"
+                },
+    "{{malaria_assessment_num_bednets_observerd}}": {"$gte": 0}
+    }
+    """
+    sort_str = """
+    {"$sort": {
+        "{{case__case_id}}": 1,
+        "{{meta_timeend}}": -1
+    }}
+    """
+    group_str = """
+    {"$group":
+    {"_id": "{{case__case_id}}",
+    "malaria_assessment_num_bednets_observerd":
+        {"$first": "${{malaria_assessment_num_bednets_observerd}}"}
+    }},
+    {"$group":
+        {"_id": null,
+        "total_num_bednets_observerd": {"$sum": "$malaria_assessment_num_bednets_observerd"}}}
+    """
+    final_str = "[%s]" % u",".join(
+        [u'{"$match": %s}' % query_str,
+         sort_str, group_str])
+
+    def __init__(self, db, dataset_id=None, dataset=None):
+        self._db = db
+        if dataset_id:
+            self.dataset = Dataset.find_one(dataset_id)
+        if dataset:
+            self.dataset = dataset
+        if not dataset_id and not dataset:
+            raise Exception(u"Please specify a dataset_id")
+
+    def get_value(self, period):
+        value = None
+        if self.dataset:
+            fields = Observation.encoding(self.dataset)
+            fields["dataset"] = self.dataset
+            fields['dataset_id_field'] = fields[DATASET_ID]
+            fields['period'] = period
+            query = json.loads(Template(self.final_str).render(fields))
+            meta_timeend = '%(meta_timeend)s' % fields
+            query[0]['$match'][meta_timeend]['$gte'] = period.start
+            query[0]['$match'][meta_timeend]['$lte'] = period.end
+            aggregate_value = self._db.observations.aggregate(query)
+            value = aggregate_value['result'][0]['total_num_bednets_observerd']
+        return value
---
+++ pydefinitions/proportion_of_under5_in_household_sleeping_under_functioning_bednets_last_night.py
@@ -0,0 +1,35 @@
+import sys
+import os
+
+from bamboo.models.dataset import Dataset
+
+sys.path.append(
+    os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..'))
+)
+from mvpqa.pydefinitions import num_u5_slept_under_bednet_last_night
+from mvpqa.pydefinitions import num_u5_slept_last_night
+
+
+class Definition(object):
+    def __init__(self, db, dataset_id=None, dataset=None):
+        self._db = db
+        if dataset_id:
+            self.dataset = Dataset.find_one(dataset_id)
+        if dataset:
+            self.dataset = dataset
+        if not dataset_id and not dataset:
+            raise Exception(u"Please specify a dataset_id")
+
+    def get_value(self, period):
+        value = None
+        def1 = num_u5_slept_under_bednet_last_night.Definition(
+            self._db, dataset=self.dataset)
+        numerator = def1.get_value(period)
+        def2 = num_u5_slept_last_night.Definition(
+            self._db, dataset=self.dataset)
+        denominator = def2.get_value(period)
+        if denominator == 0:
+            value = 0
+        else:
+            value = round(100 * (float(numerator) / float(denominator)), 2)
+        return value, numerator, denominator
