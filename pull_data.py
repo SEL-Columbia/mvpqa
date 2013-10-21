4afad6d2b039d13ddd468e357dc836dcb8930393
Ukang'a Dickson <ukanga@gmail.com>
2013-10-21 13:41:35 +0300

    download pregnancy visits data

--- pull_data.py
+++ pull_data.py
@@ -8,6 +8,7 @@ from urlparse import urljoin
 from zipfile import ZipFile
 
 from config.settings import COMMCARE_URL, COMMCARE_USERNAME, COMMCARE_PASSWORD
+from utils import get_report_id
 
 AUTH = HTTPDigestAuth(COMMCARE_USERNAME, COMMCARE_PASSWORD)
 
@@ -45,6 +46,34 @@ def download_cases(domain):
             print u"Successfully downloaded case_export_all.csv"
 
 
+def download_custom_reports(domain, report_id, report_name):
+    """Downloads Pregnancy Visits data csv zip export from commcare.
+    The extracted csv is placed in data/DOMAIN/latest/Pregnancy Visit.csv.
+    :domain - commcare domain
+    :report_id - custom report id for report
+    """
+    if not report_id:
+        raise Exception(u"Please ensure the pregnancy custom report id"
+                        u" is configured in CUSTOM_REPORTS settings.")
+    url_path = "/a/%(domain)s/reports/export/custom/%(id)s/download/" % {
+        'domain': domain, 'id': report_id}
+    f = download_from_commcare(url_path)
+    z = ZipFile(f, 'r')
+    zdst = os.path.join('data', domain, 'latest')
+    dst = os.path.join(zdst, report_name)
+    if z.NameToInfo.keys():
+        filename = z.NameToInfo.keys()[0]
+        src = os.path.join(zdst, filename)
+        z.extract(z.NameToInfo[filename], zdst)
+        if os.path.exists(src):
+            os.rename(src, dst)
+            print u"Successfully downloaded %s" % report_name
+
+
+def download_pregancy_visits(domain, report_id):
+    download_custom_reports(domain, report_id, "Pregnancy Visit.csv")
+
+
 if __name__ == '__main__':
     arguments = sys.argv[1:]
     domain = what = None
@@ -58,7 +87,10 @@ if __name__ == '__main__':
         exit(0)
     if what == 'cases' or what == 'case':
         download_cases(domain)
+    elif what == 'pregnancy-visit':
+        report_id = get_report_id(domain, what)
+        download_pregancy_visits(domain, report_id)
     else:
         print (u"Missing or Incorrect action specified!"
-               u" Expected cases, pregnancy, ...\ni.e\n$"
+               u" Expected cases, pregnancy-visit, ...\ni.e\n$"
                u"python pull_data.py mvp-ruhiira cases")
