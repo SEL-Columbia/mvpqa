d2e002cc57a0263009dd6023675eead96a9a8e58
Ukang'a Dickson <ukanga@gmail.com>
2013-10-23 09:49:00 +0300

    include household, pregnancy and child visit form xlmns

--- config/settings.py
+++ config/settings.py
@@ -17,13 +17,19 @@ CUSTOM_REPORTS = {
 FDURL = 'http://openrosa.org/formdesigner/'
 FORM_XMLNS = {
     'mvp-ruhiira': {
+        'child-visit': FDURL + 'B9CEFDCD-8068-425F-BA67-7DC897030A5A',
         'child-close': FDURL + 'AC164B28-AECA-45C9-B7F6-E0668D5AF84B',
+        'household-visit': FDURL + '266AD1A0-9EAE-483E-B4B2-4E85D6CA8D4B',
         'pregnancy-outcome': FDURL + '01EB3014-71CE-4EBE-AE34-647EF70A55DE',
+        'pregnancy-visit': FDURL + '185A7E63-0ECD-4D9A-8357-6FD770B6F065',
         'death': FDURL + 'b3af1fddeb661ee045fef1e764995440ea8f057f',
     },
     'mvp-sauri': {
+        'child-visit': FDURL + 'B9CEFDCD-8068-425F-BA67-7DC897030A5A',
         'child-close': FDURL + 'AC164B28-AECA-45C9-B7F6-E0668D5AF84B',
+        'household-visit': FDURL + '266AD1A0-9EAE-483E-B4B2-4E85D6CA8D4B',
         'pregnancy-outcome': FDURL + '01EB3014-71CE-4EBE-AE34-647EF70A55DE',
+        'pregnancy-visit': FDURL + '185A7E63-0ECD-4D9A-8357-6FD770B6F065',
         'death': FDURL + 'b3af1fddeb661ee045fef1e764995440ea8f057f',
     }
 }
