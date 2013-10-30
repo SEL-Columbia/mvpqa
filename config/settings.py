bdafacfc27e7a2b0e4d4749baee3bcc74f02a3c8
Ukang'a Dickson <ukanga@gmail.com>
2013-10-30 16:41:26 +0300

    config for mwandama, pampaida, tiby

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,22 @@ CUSTOM_REPORTS = {
         'household-visit': '5d63706d5d62517a638926f2c59be20d',
         'child-visit': '51d3d33f94c4f7654e81ac443a59344d'
     },
+    'mvp-mwandama': {
+        'pregnancy-visit': '0481c2222ebfdb322b3e8115732c7507',
+        'household-visit': '5d63706d5d62517a638926f2c59be20d',
+        'child-visit': '51d3d33f94c4f7654e81ac443a59344d'
+    },
     'mvp-bonsaaso': {
+        'pregnancy-visit': '0481c2222ebfdb322b3e8115732c7507',
+        'household-visit': '5d63706d5d62517a638926f2c59be20d',
+        'child-visit': '51d3d33f94c4f7654e81ac443a59344d'
+    },
+    'mvp-pampaida': {
+        'pregnancy-visit': '0481c2222ebfdb322b3e8115732c7507',
+        'household-visit': '5d63706d5d62517a638926f2c59be20d',
+        'child-visit': '51d3d33f94c4f7654e81ac443a59344d'
+    },
+    'mvp-tiby': {
         'pregnancy-visit': '0481c2222ebfdb322b3e8115732c7507',
         'household-visit': '5d63706d5d62517a638926f2c59be20d',
         'child-visit': '51d3d33f94c4f7654e81ac443a59344d'
@@ -60,7 +75,31 @@ FORM_XMLNS = {
         'pregnancy-visit': FDURL + '185A7E63-0ECD-4D9A-8357-6FD770B6F065',
         'death': FDURL + 'b3af1fddeb661ee045fef1e764995440ea8f057f',
     },
+    'mvp-mwandama': {
+        'child-visit': FDURL + 'B9CEFDCD-8068-425F-BA67-7DC897030A5A',
+        'child-close': FDURL + 'AC164B28-AECA-45C9-B7F6-E0668D5AF84B',
+        'household-visit': FDURL + '266AD1A0-9EAE-483E-B4B2-4E85D6CA8D4B',
+        'pregnancy-outcome': FDURL + '01EB3014-71CE-4EBE-AE34-647EF70A55DE',
+        'pregnancy-visit': FDURL + '185A7E63-0ECD-4D9A-8357-6FD770B6F065',
+        'death': FDURL + 'b3af1fddeb661ee045fef1e764995440ea8f057f',
+    },
     'mvp-bonsaaso': {
+        'child-visit': FDURL + 'B9CEFDCD-8068-425F-BA67-7DC897030A5A',
+        'child-close': FDURL + 'AC164B28-AECA-45C9-B7F6-E0668D5AF84B',
+        'household-visit': FDURL + '266AD1A0-9EAE-483E-B4B2-4E85D6CA8D4B',
+        'pregnancy-outcome': FDURL + '01EB3014-71CE-4EBE-AE34-647EF70A55DE',
+        'pregnancy-visit': FDURL + '185A7E63-0ECD-4D9A-8357-6FD770B6F065',
+        'death': FDURL + 'b3af1fddeb661ee045fef1e764995440ea8f057f',
+    },
+    'mvp-tiby': {
+        'child-visit': FDURL + 'B9CEFDCD-8068-425F-BA67-7DC897030A5A',
+        'child-close': FDURL + 'AC164B28-AECA-45C9-B7F6-E0668D5AF84B',
+        'household-visit': FDURL + '266AD1A0-9EAE-483E-B4B2-4E85D6CA8D4B',
+        'pregnancy-outcome': FDURL + '01EB3014-71CE-4EBE-AE34-647EF70A55DE',
+        'pregnancy-visit': FDURL + '185A7E63-0ECD-4D9A-8357-6FD770B6F065',
+        'death': FDURL + 'b3af1fddeb661ee045fef1e764995440ea8f057f',
+    },
+    'mvp-pampaida': {
         'child-visit': FDURL + 'B9CEFDCD-8068-425F-BA67-7DC897030A5A',
         'child-close': FDURL + 'AC164B28-AECA-45C9-B7F6-E0668D5AF84B',
         'household-visit': FDURL + '266AD1A0-9EAE-483E-B4B2-4E85D6CA8D4B',
