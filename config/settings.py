5500152d00525210165c13daee0f1174dad63941
Ukang'a Dickson <ukanga@gmail.com>
2013-11-04 08:44:55 +0000

    pampaida form xmlns settings

--- config/settings.py
+++ config/settings.py
@@ -105,12 +105,12 @@ FORM_XMLNS = {
         'death': FDURL + '45AEF3B4-AA47-4896-8A23-B99577976554',
     },
     'mvp-pampaida': {
-        'child-visit': FDURL + 'B9CEFDCD-8068-425F-BA67-7DC897030A5A',
-        'child-close': FDURL + 'AC164B28-AECA-45C9-B7F6-E0668D5AF84B',
-        'household-visit': FDURL + '266AD1A0-9EAE-483E-B4B2-4E85D6CA8D4B',
-        'pregnancy-outcome': FDURL + '01EB3014-71CE-4EBE-AE34-647EF70A55DE',
-        'pregnancy-visit': FDURL + '185A7E63-0ECD-4D9A-8357-6FD770B6F065',
-        'death': FDURL + 'b3af1fddeb661ee045fef1e764995440ea8f057f',
+        'child-visit': FDURL + '99EB4DCB-891C-49AA-9445-ADCD87ECD88B',
+        'child-close': FDURL + 'BD1169E0-1A7A-4077-A25A-AAF77D8359CE',
+        'household-visit': FDURL + 'F3EB5B63-17B9-4B37-ABF7-7DB6CD3B108E',
+        'pregnancy-outcome': FDURL + '6A3B04EF-EB3D-4C92-A577-E7A76F43B1B7',
+        'pregnancy-visit': FDURL + '5567CDF9-464B-444E-ADEA-0850CAB5B2E2',
+        'death': FDURL + 'EC16CF26-6121-49FE-A5D6-1C833769517D',
     },
     'mvp-sada': {
         'child-visit': FDURL + 'B9CEFDCD-8068-425F-BA67-7DC897030A5A',
